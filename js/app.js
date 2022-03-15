dab0ea55397098e7158580d7eefb1adc3841f47a
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-15 11:26:06 +0300

    Display books in local storage in Book List

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
 // All the lovely javascript comes here
 
-const currentBooks = document.querySelector('#display-book');
 const addbookBtn = document.querySelector('#add-book');
 const bookTitle = document.querySelector('#title');
 const bookAuthor = document.querySelector('#author');
 const bookList = JSON.parse(localStorage.getItem('library')) || [];
 
-function addBook(bookTitle, bookAuthor) {
+const addBook = (bookTitle, bookAuthor) => {
   const book = {};
   book.title = bookTitle.value;
   book.author = bookAuthor.value;
   bookList.push(book);
-}
+};
 
 addbookBtn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -22,19 +21,31 @@ addbookBtn.addEventListener('click', (e) => {
   localStorage.setItem('library', JSON.stringify(bookList));
 });
 
-// function updateBookList() {
-//   if (localStorage.getItem('library')) {
-//     // display current books
-//   }
-// }
-// window.onload = updateBookList;
+const displayBook = (book) => {
+  const {
+    title,
+    author,
+  } = book;
 
-// function displayBooks() {
-//   currentBooks.innerHTML = `
-//     <div>
-//     <p>${bookList}</p>
-//     <div>
-//   `;
-// }
+  const bookContainer = document.createElement('div');
+  bookContainer.className = 'book';
+  bookContainer.innerHTML = `
+    <p>${title}</p>
+    <p>${author} </p>
+    <button class="delete">Delete Book</button>
+  `;
 
-// window.onload = displayBooks;
+  return bookContainer;
+};
+
+const displayBooks = () => {
+  const currentBooks = document.querySelector('#books-list');
+  const libraryContainer = document.createElement('div');
+
+  bookList.forEach((book) => {
+    libraryContainer.appendChild(displayBook(book));
+    currentBooks.appendChild(libraryContainer);
+  });
+};
+
+window.onload = displayBooks;
