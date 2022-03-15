fa15d5056fb1d04756158c49232506d4365098d6
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-15 16:34:13 +0300

    Refactor code to use class based

--- js/app.js
+++ js/app.js
@@ -1,53 +1,19 @@
-// All the lovely javascript comes here
-
-const addbookBtn = document.querySelector('#add-book');
-const bookTitle = document.querySelector('#title');
-const bookAuthor = document.querySelector('#author');
-const bookList = JSON.parse(localStorage.getItem('book-library')) || [];
-
-const displayBook = (book) => {
-  const {
-    title,
-    author,
-  } = book;
-
-  const bookContainer = document.createElement('div');
-  bookContainer.className = 'book';
-  bookContainer.innerHTML = `
-    <p>${title}</p>
-    <p>${author} </p>
-    <button class="delete">Delete Book</button>
-    <hr class="line-break">
-  `;
-
-  return bookContainer;
-};
-
-const displayBooks = () => {
-  const currentBooks = document.querySelector('#books-list');
-  const libraryContainer = document.createElement('div');
-
-  bookList.forEach((book) => {
-    libraryContainer.appendChild(displayBook(book));
-    currentBooks.appendChild(libraryContainer);
-  });
-};
-
-window.onload = displayBooks;
-
-const addBook = (bookTitle, bookAuthor) => {
-  const book = {};
-  book.title = bookTitle.value;
-  book.author = bookAuthor.value;
-  bookList.push(book);
-  displayBooks();
-};
-
-addbookBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (bookTitle.value === null || bookAuthor.value === null) {
-    return;
+// Book Class: Represents a Book
+class Book {
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = new Date();
   }
-  addBook(bookTitle, bookAuthor);
-  localStorage.setItem('book-library', JSON.stringify(bookList));
-});
+}
+
+// UI Class: Handle UI
+class
+
+// Store Class: Handles book storage
+
+// Event: Display Book
+
+// Event: Add a Book
+
+// Event: Remove a Book
